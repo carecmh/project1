9eb3a6c4498a5db50555aecbd4d0faf32da50e6d
CAROLINE HAMON <CMH20D@FSUEDU>
2023-09-26 21:54:31 -0400

    Buttons, attempted inputing image, title

--- sketch.js
+++ sketch.js
@@ -1,7 +1,28 @@
-function setup() {
-  createCanvas(400, 400);
+var myParagraph;
+var paragraghTwo;
+var myButton;
+var buttontwo;
+var img;
+
+function preload() {
+  img = loadImage("assets/IMG_6749.jpg")
 }
 
+function setup() {
+  myParagraph = createP("BeReal.");
+  myParagraph.position(150,30);
+
+  paragraghTwo = createP("Like this photo to move on to the next.")
+  paragraghTwo.position(60,330);
+ 
+  myButton = createButton("Like");
+  myButton.position(100, 300);
+
+  buttontwo = createButton("Dislike");
+  buttontwo.position (200, 300);
+}
+
+
 function draw() {
-  background(125);
+image(img, 100, 200)
 }
